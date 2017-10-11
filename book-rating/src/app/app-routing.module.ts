bcdcb1f2737f2ec7ed947fc860009dfb9c481c32
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2017-10-11 17:19:15 +0200

    routing!

--- book-rating/src/app/app-routing.module.ts
+++ book-rating/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { BookDetailsComponent } from './book-details/book-details.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent
+  },
+  {
+    path: 'book/:isbn',
+    component: BookDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ book-rating/src/app/book-details/book-details.component.ts
@@ -0,0 +1,20 @@
+import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'br-book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.scss']
+})
+export class BookDetailsComponent implements OnInit {
+
+  isbn: string;
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    // this.isbn = this.route.snapshot.params.isbn;
+    this.route.params.subscribe(params => this.isbn = params.isbn);
+  }
+
+}
